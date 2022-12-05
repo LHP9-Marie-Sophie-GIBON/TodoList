b3024fa4898f9a1077d60e9daace13ea20b65dae
MarieSophieGB <mariesophie.gb@gmail.com>
2022-12-05 11:01:35 +0100

    finito

--- assets/assets/app.js
+++ assets/assets/app.js
@@ -10,6 +10,14 @@ function SaveInLocalStorage() {
     const TodoList = [];
     newItems.forEach(element => { TodoList.push(element.value); })
 
+    let ok = document.querySelectorAll('.complete');
+    const okArray = [];
+    ok.forEach(element => {
+        okArray.push(element.className);
+    })
+
+
+    localStorage.setItem('okArray', JSON.stringify(okArray));
     localStorage.setItem('TodoList', JSON.stringify(TodoList));
 }
 
@@ -17,7 +25,7 @@ function SaveInLocalStorage() {
 Ajout.addEventListener('click', function () {
     let newbtnGroup = document.createElement('fieldset');
     newbtnGroup.innerHTML = `<div class="btn-group" role="group">
-    <button type="button" class="btn m-3"><img class="ok" src="https://img.icons8.com/color-glass/28/null/checkmark--v1.png"/> </button>    
+    <button type="button" class="btn m-3"><img class="ok complete" src="https://img.icons8.com/color-glass/28/null/checkmark--v1.png"/> </button>    
     <input type="text" class="newItem form-control m-4" placeholder="" value="${item.value}" disabled/>
     <button type="button" class="btn m-3"><img class="Editer" src="https://img.icons8.com/color-glass/38/null/edit--v1.png"/> </button>    
     <button type="button" class="btn m-3"><img class="Supprimer" src="https://img.icons8.com/fluency/38/null/delete-forever.png"/> </button> 
@@ -42,12 +50,12 @@ window.addEventListener("click", event => {
         event.target.src = 'https://img.icons8.com/color-glass/38/null/edit--v1.png';
     }
     // bouton done
-    if (event.target.classList == "ok") {
-        event.target.className = "notok"
+    if (event.target.classList == "ok complete") {
+        event.target.className = "notok complete"
         event.target.src = 'https://img.icons8.com/color-glass/28/null/double-tick--v1.png'
-        event.target.parentNode.parentNode.children[1].className = "text-white fw-bold bg-secondary newItem form-control m-4";
-    } else if (event.target.classList == "notok") {
-        event.target.className = "ok"
+        event.target.parentNode.parentNode.children[1].className = "newItem form-control m-4 lineThrough";
+    } else if (event.target.classList == "notok complete") {
+        event.target.className = "ok complete"
         event.target.src = 'https://img.icons8.com/color-glass/28/null/checkmark--v1.png'
         event.target.parentNode.parentNode.children[1].className = "newItem form-control m-4";
 
@@ -58,16 +66,22 @@ window.addEventListener("click", event => {
 // reload le contenu de la page au chargement
 document.addEventListener('DOMContentLoaded', function () {
     let local = JSON.parse(localStorage.getItem('TodoList'));
+    let local2 = JSON.parse(localStorage.getItem('okArray'));
+    let i = 0;
 
     local.forEach(element => {
         let newbtnGroup = document.createElement('fieldset');
         newbtnGroup.innerHTML = `<div class="btn-group" role="group">
-    <button type="button" class="btn m-3"><img class="ok" src="https://img.icons8.com/color-glass/28/null/checkmark--v1.png"/> </button>    
-    <input type="text" class="newItem form-control m-4" placeholder="" value="${element}" disabled/>
+    <button type="button" class="btn m-3"><img class="${local2[i]}" src="${local2[i] == 'ok complete' ? "https://img.icons8.com/color-glass/28/null/checkmark--v1.png" : "https://img.icons8.com/color-glass/28/null/double-tick--v1.png"}"/> </button>    
+    <input type="text" class="newItem ${local2[i] == 'notok complete' ? 'lineThrough' : ''} form-control m-4" placeholder="" value="${element}" disabled/>
     <button type="button" class="btn m-3"><img class="Editer" src="https://img.icons8.com/color-glass/38/null/edit--v1.png"/> </button>    
     <button type="button" class="btn m-3"><img class="Supprimer" src="https://img.icons8.com/fluency/38/null/delete-forever.png"/> </button> 
     </div>`
         document.querySelector('#btnGroup').appendChild(newbtnGroup);
         SaveInLocalStorage()
+        i++;
     })
+
+
+
 });
